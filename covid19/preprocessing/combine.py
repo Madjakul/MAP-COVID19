98f4e183090dc964c6166a34c692937bd747a3c0
Madjakul <madjakul@outlook.com>
2020-09-14 18:28:00 +0200

    Merge et aggregation réussi !

--- covid19/preprocessing/combine.py
+++ covid19/preprocessing/combine.py
@@ -10,17 +10,26 @@ class Merge():
     @staticmethod
     def run():
         hopkinsDf = Util("assets/time_series_covid19_confirmed_global.csv")
-        hopkinsDf.delete_columns(["Province/State"])
-        hopkinsDf.melt(["Country/Region", "Lat", "Long"], "Date")
-        hopkinsDf.to_datetime("Date")
-        hopkinsDf.sort(["Country/Region", "Date"])
+        hopkinsDf.df["Country/Region"].replace("US", "United States", inplace=True)
+        hopkinsDf.df.to_csv('assets/test.csv', encoding='utf-8')
+        hopkinsDf.aggregate_sum("Country/Region")
+        hopkinsDf.delete_columns(["Lat", "Long"])
+        hopkinsDf.melt(["Country/Region"], "Date") # Transforme les colonnes en ligne, le tout par pays
+        hopkinsDf.to_datetime("Date") # Transforme la colonne date du dataframe en datetime64
+        hopkinsDf.sort(["Country/Region", "Date"]) # classe le dataframe par pays et par date
         hopkinsDf.rename_column("Country/Region", "Country_Region")
         hopkinsDf.translate_countries("Country_Region") # Traduit le pays de l'anglais au français
-        print(hopkinsDf)
+        hopkinsDf.create_primary_key("Country_FR", "Date") #Crée une clé primaire artificielle à partire des deux colonnes citées
 
         covidDf = Util("assets/df_covid_20aug.csv")
         covidDf.delete_columns(["Num_ligne", "Id"])
         covidDf.to_datetime("Date")
-        covidDf.sort(["Pays_Ou_Entites", "Date"])
+        covidDf.sort(["Pays_Ou_Entités", "Date"])
+        covidDf.create_primary_key("Pays_Ou_Entités", "Date")
 
-        print(covidDf.df.Pays_Ou_Entites .nunique())
+        mergedDf = hopkinsDf + covidDf # Merge les deux bases de données en fonction d'une clé primaire
+        mergedDf.drop(columns=["Pays_Ou_Entités"], axis=1, inplace=True)
+        mergedDf.rename(columns = {"value":"Total_cas_confirmés_EN"}, inplace = True)
+        mergedDf.rename(columns = {"Country_Region":"Pays_Ou_Entités_EN"}, inplace = True)
+        mergedDf.rename(columns = {"Country_FR":"Pays_Ou_Entités_TRAD"}, inplace = True)
+        mergedDf.to_csv('assets/exit.csv', encoding='utf-8')
--- covid19/preprocessing/process.py
+++ covid19/preprocessing/process.py
@@ -21,11 +21,15 @@ class Util:
     def write(self):
         self.df.to_csv('assets/exit.csv', encoding='utf-8')
 
+    def aggregate_sum(self, id):
+        # Fais la somme de chaque ligne en fonction du pays
+        self.df = self.df.groupby(id, as_index=False).sum()
+
     def rename_column(self, oldName, newName):
         self.df.rename(columns = {oldName:newName}, inplace = True)
 
     def delete_columns(self, columns):
-        self.df.drop(columns, axis=1, inplace=True)
+        self.df.drop(columns=columns, axis=1, inplace=True)
     
     def translate_countries(self, column):
         countryList = []
@@ -34,8 +38,12 @@ class Util:
             [countryList.append(i) for i in translator.translate(country)]
         self.df["Country_FR"] = countryList
 
-    def get_alpha2(self):
-        pass
+    def create_primary_key(self, column1, column2):
+        # Combine deux labels afin de créer une clé primaire pour un dataframe
+        self.df["Primary_Key"] = self.df[column1].astype(str) + self.df[column2].astype(str)
+
+    def __add__(self, other):
+        return pd.merge(self.df, other.df)
 
     def __str__(self):
         return (str(self.df.info()) + "\n" + str(self.df))
